830dfc44b67a65420f072f08f9d05ba17296ec3f
moliver4 <tifftang22@gmail.com>
2020-03-11 12:42:06 -0700

    file upload via local storage with multer

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controllers');
-
+const fileUpload = require('../middleware/file-upload')
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
 router.post(
     '/signup',
+    //middleware to reteive a single file, parameter is key or name of incoming file. we expect an image key
+    fileUpload.single('image'),
     [
       check('name')
         .not()
